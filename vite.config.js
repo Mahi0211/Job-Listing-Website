bd28be5e8b49a9e7535cc16efa33115edcab7005
Mahi0211 <97247845+Mahi0211@users.noreply.github.com>
2024-07-03 18:00:37 +0530

    proxying

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Change this to 'build' if you need
+    outDir: "build", // Change this to 'build' if you need
   },
   server: {
     port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrgin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
