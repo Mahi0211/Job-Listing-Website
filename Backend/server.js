8ca6b631248fd9a3453899314945a1e6c4f48c8b
Mahi0211 <mahinpranave@gmail.com>
2025-02-11 20:35:47 +0530

    Final commit - seperate backend and deploy

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,4 @@
 import express from "express";
-// import { fileURLToPath } from "url";
-// import path from "path";
 import jobs from "./routes/jobs.js";
 import logger from "./middleware/logger.js";
 import notFound from "./middleware/notFound.js";
@@ -9,14 +7,11 @@ import cors from "cors";
 
 const PORT = process.env.PORT || 8080;
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
 const app = express();
 
 app.use(cors());
 
-// Body parser missleware
+// Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
